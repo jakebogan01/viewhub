d67e8a65294d09a33360a8929495401a0e0739a5
Jake Bogan <jakebogan01@gmail.com>
2023-08-17 15:53:54 -0400

    adds user and task relationship

--- resources/js/Pages/Tasks/Show.svelte
+++ resources/js/Pages/Tasks/Show.svelte
@@ -15,9 +15,13 @@
 
 <div class="space-y-4 mt-5">
     <div class="border border-gray-200 rounded p-4 my-2">
-        <h2 class="text-2xl font-bold pb-3">{task.title}</h2>
+        <div class="flex justify-between items-center">
+            <h2 class="text-2xl font-bold pb-3">{task.title}</h2>
+
+            <span>Created by: {task.user}</span>
+        </div>
         <p>{task.description}</p>
-        <a use:inertia href="/" class="inline-block mt-2 text-blue-500 border border-gray-200 px-4 py-1 rounded-lg bg-white">{task.tag.name}</a>
+        <a use:inertia href="/" class="inline-block mt-2 text-blue-500 border border-gray-200 px-4 py-1 rounded-lg bg-white">{task.tag}</a>
     </div>
 </div>
 
