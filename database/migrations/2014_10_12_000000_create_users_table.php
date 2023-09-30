cc769f5269b3b78f4189dab5e44e02cbd955e09d
Jake Bogan <jake@Jakes-MacBook-Air.local>
2023-09-30 16:41:21 -0400

    adds admin middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('dark_mode')->default(0);
+            $table->boolean('is_admin')->default(0);
             $table->string('timezone')->nullable();
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Dashboard\DashboardController;
-use App\Http\Controllers\Dashboard\SettingsController;
+use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\SettingsController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\Shared\CommentController;
 use App\Http\Controllers\Shared\ImageController;
@@ -15,43 +15,57 @@ Route::get('/', function() {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::group([
+        'prefix' => 'admin',
+        'middleware' => 'is_admin',
+        'as' => 'admin.',
+        ], function () {
+            Route::get('/test', function() {
+                return Inertia::render('Test');
+            });
+    });
 
-    // dashboard
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-    Route::get('/dashboard/tasks/{task:slug}', [DashboardController::class, 'show'])->name('dashboard.show');
-    Route::get('/dashboard/task/create', [DashboardController::class, 'create'])->name('dashboard.create');
-    Route::post('/dashboard/task/create', [DashboardController::class, 'store']);
-    Route::get('/dashboard/task/{task:slug}/edit', [DashboardController::class, 'edit']);
-    Route::patch('/dashboard/task/{task}', [DashboardController::class, 'update']);
-    Route::delete('/dashboard/task/{task}', [DashboardController::class, 'destroy']);
-    Route::get('/dashboard/projects', [DashboardController::class, 'allProjects'])->name('dashboard.projects');
+    Route::group([
+//        'prefix' => 'client',
+//        'as' => 'client.',
+    ], function () {
+        // dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+        Route::get('/dashboard/tasks/{task:slug}', [DashboardController::class, 'show'])->name('dashboard.show');
+        Route::get('/dashboard/task/create', [DashboardController::class, 'create'])->name('dashboard.create');
+        Route::post('/dashboard/task/create', [DashboardController::class, 'store']);
+        Route::get('/dashboard/task/{task:slug}/edit', [DashboardController::class, 'edit']);
+        Route::patch('/dashboard/task/{task}', [DashboardController::class, 'update']);
+        Route::delete('/dashboard/task/{task}', [DashboardController::class, 'destroy']);
+        Route::get('/dashboard/projects', [DashboardController::class, 'allProjects'])->name('dashboard.projects');
 
-    // images
-    Route::post('/dashboard/image/upload', [ImageController::class, 'upload']);
-    Route::delete('/dashboard/image/revert/{folder}', [ImageController::class, 'revert']);
-    Route::delete('/dashboard/image/delete/{image}', [ImageController::class, 'destroy']);
+        // images
+        Route::post('/dashboard/image/upload', [ImageController::class, 'upload']);
+        Route::delete('/dashboard/image/revert/{folder}', [ImageController::class, 'revert']);
+        Route::delete('/dashboard/image/delete/{image}', [ImageController::class, 'destroy']);
 
-    // settings
-    Route::get('/dashboard/settings', [SettingsController::class, 'index'])->name('settings.index');
-    Route::patch('/dashboard/settings/update/information', [SettingsController::class, 'updateinformation']);
-    Route::patch('/dashboard/settings/update/password', [SettingsController::class, 'updatePassword']);
-    Route::delete('/dashboard/settings/delete/avatar', [SettingsController::class, 'deleteAvatar']);
-    Route::delete('/dashboard/settings/delete/account', [SettingsController::class, 'deleteAccount']);
-    Route::post('/dashboard/settings/create/review', [SettingsController::class, 'store']);
+        // settings
+        Route::get('/dashboard/settings', [SettingsController::class, 'index'])->name('settings.index');
+        Route::patch('/dashboard/settings/update/information', [SettingsController::class, 'updateinformation']);
+        Route::patch('/dashboard/settings/update/password', [SettingsController::class, 'updatePassword']);
+        Route::delete('/dashboard/settings/delete/avatar', [SettingsController::class, 'deleteAvatar']);
+        Route::delete('/dashboard/settings/delete/account', [SettingsController::class, 'deleteAccount']);
+        Route::post('/dashboard/settings/create/review', [SettingsController::class, 'store']);
 
-    // comments
-    Route::post('/dashboard/comment/create', [CommentController::class, 'storeComment']);
-    Route::post('/dashboard/comment/reply/create', [CommentController::class, 'storeReply']);
-    Route::post('/dashboard/comment/comment-reply/create', [CommentController::class, 'storeCommentReply']);
-    Route::delete('/dashboard/comment/{comment}', [CommentController::class, 'destroyComment']);
-    Route::delete('/dashboard/comment/reply/{reply}', [CommentController::class, 'destroyReply']);
-    Route::patch('/dashboard/comment/{comment}', [CommentController::class, 'updateComment']);
-    Route::patch('/dashboard/comment/reply/{reply}', [CommentController::class, 'updateReply']);
+        // comments
+        Route::post('/dashboard/comment/create', [CommentController::class, 'storeComment']);
+        Route::post('/dashboard/comment/reply/create', [CommentController::class, 'storeReply']);
+        Route::post('/dashboard/comment/comment-reply/create', [CommentController::class, 'storeCommentReply']);
+        Route::delete('/dashboard/comment/{comment}', [CommentController::class, 'destroyComment']);
+        Route::delete('/dashboard/comment/reply/{reply}', [CommentController::class, 'destroyReply']);
+        Route::patch('/dashboard/comment/{comment}', [CommentController::class, 'updateComment']);
+        Route::patch('/dashboard/comment/reply/{reply}', [CommentController::class, 'updateReply']);
 
-    // miscellaneous
-    Route::post('/update-dark-mode', [DashboardController::class, 'enableDarkMode']);
-    Route::post('/notification-mark-read', [NotificationController::class, 'markRead']);
-    Route::post('/notification/{task}/like', [NotificationController::class, 'toggle']);
+        // miscellaneous
+        Route::post('/update-dark-mode', [DashboardController::class, 'enableDarkMode']);
+        Route::post('/notification-mark-read', [NotificationController::class, 'markRead']);
+        Route::post('/notification/{task}/like', [NotificationController::class, 'toggle']);
+    });
 });
 
 Route::get('/about', function () {
